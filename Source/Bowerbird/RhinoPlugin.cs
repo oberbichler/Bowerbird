4a4fc696d02f02cca4e47994ec829bb1f42ecba5
Thomas J. Oberbichler <thomas.oberbichler@gmail.com>
2014-09-19 17:11:18 +0200

    Add RhinoPlugin

---
+++ Source/Bowerbird/RhinoPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowerbird
+{
+    public class BowerbirdRhinoPlugIn : Rhino.PlugIns.PlugIn
+    {
+        public BowerbirdRhinoPlugIn()
+        {
+            Instance = this;
+        }
+
+        public static BowerbirdRhinoPlugIn Instance { get; private set; }
+    }
+}
