d46c509182ce8c86efa1fd838c896868231ee2e1
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-08-25 19:59:06 +0200

    Fix SolutionDirectory

--- build.cmd
+++ build.cmd
@@ -3,6 +3,6 @@ cls
 
 nuget install FAKE -Version 3.26.7 -o packages\ -ExcludeVersion
 
-nuget restore -SolutionDirectory .\Source\
+nuget restore -SolutionDirectory C:\projects\bowerbird\Source\
 
 packages\FAKE\tools\FAKE.exe build.fsx
