1686f965fc16e1a23ef36f2338aa7159ed46d672
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-12-07 19:22:51 +0100

    Revert "Merge branch 'master' into feature/refactor-paths"
    This reverts commit 9a8d2df9287112d55f91c2876df421510764f7ed.

---
+++ Bowerbird/Components/BBGeodesicTorsionPathComponent.cs
@@ -0,0 +1,55 @@
+﻿using Bowerbird.Parameters;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowerbird.Components
+{
+    public class BBGeodesicTorsionPathComponent : GH_Component
+    {
+        public BBGeodesicTorsionPathComponent() : base("BB Geodesic Torsion Path", "BBGTrs", "Beta! Interface might change!", "Bowerbird", "Paths")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Value", "V", "", GH_ParamAccess.item, 0.0);
+            pManager.AddNumberParameter("Angle", "A", "", GH_ParamAccess.item, 0.0);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddParameter(new PathParameter(), "Path", "P", "", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // --- Input
+
+            var value = default(double);
+            var angle = default(double);
+
+            DA.GetData(0, ref value);
+            DA.GetData(1, ref angle);
+
+            // --- Execute
+
+            var path = GeodesicTorsionPath.Create(angle, value);
+
+            // --- Output
+
+            DA.SetData(0, path);
+        }
+
+        protected override Bitmap Icon => Properties.Resources.icon_principal_path;
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+        public override Guid ComponentGuid => new Guid("{E540E209-EF00-4E66-9460-CC62C1D394AB}");
+    }
+}
