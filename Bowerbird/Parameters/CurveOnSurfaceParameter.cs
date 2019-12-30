8e3917ce0498ea6705ee8ef1d9b034a023671aea
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-12-30 18:18:31 +0100

    Add CurveOnSurfaceParameter

---
+++ Bowerbird/Parameters/CurveOnSurfaceParameter.cs
@@ -0,0 +1,15 @@
+﻿using Bowerbird.Types;
+using Grasshopper.Kernel;
+using System;
+
+namespace Bowerbird.Parameters
+{
+    internal class CurveOnSurfaceParameter : GH_Param<GH_CurveOnSurface>
+    {
+        public CurveOnSurfaceParameter() : base(new GH_InstanceDescription("BB Path", "BBPath", "", "Bowerbird", "CurveOnSurface"))
+        {
+        }
+
+        public override Guid ComponentGuid => new Guid("{84DD6CB7-9DC6-4EF4-B46B-45F37361F7F1}");
+    }
+}
