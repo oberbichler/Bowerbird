1741b67773034d68f762a097bdac312034830845
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-01-08 11:31:09 +0100

    Enable preview

--- Bowerbird/Parameters/CurveOnSurfaceParameter.cs
+++ Bowerbird/Parameters/CurveOnSurfaceParameter.cs
@@ -1,15 +1,28 @@
 ﻿using Bowerbird.Types;
 using Grasshopper.Kernel;
+using Rhino.Geometry;
 using System;
 
 namespace Bowerbird.Parameters
 {
-    internal class CurveOnSurfaceParameter : GH_Param<GH_CurveOnSurface>
+    internal class CurveOnSurfaceParameter : GH_Param<GH_CurveOnSurface>, IGH_PreviewObject
     {
         public CurveOnSurfaceParameter() : base(new GH_InstanceDescription("BB Path", "BBPath", "", "Bowerbird", "CurveOnSurface"))
         {
         }
 
         public override Guid ComponentGuid => new Guid("{84DD6CB7-9DC6-4EF4-B46B-45F37361F7F1}");
+
+        // --- IGH_PreviewObject
+
+        public bool Hidden { get; set; }
+
+        public bool IsPreviewCapable => true;
+
+        public BoundingBox ClippingBox => Preview_ComputeClippingBox();
+
+        public void DrawViewportMeshes(IGH_PreviewArgs args) => Preview_DrawMeshes(args);
+
+        public void DrawViewportWires(IGH_PreviewArgs args) => Preview_DrawWires(args);
     }
 }
--- Bowerbird/Types/GH_CurveOnSurface.cs
+++ Bowerbird/Types/GH_CurveOnSurface.cs
@@ -1,4 +1,8 @@
-﻿using Grasshopper.Kernel.Types;
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino;
+using Rhino.DocObjects;
+using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Bowerbird.Types
 {
-    public class GH_CurveOnSurface : GH_Goo<CurveOnSurface>
+    public class GH_CurveOnSurface : GH_GeometricGoo<CurveOnSurface>, IGH_PreviewData, IGH_BakeAwareData
     {
         public GH_CurveOnSurface() : base()
         {
@@ -17,11 +21,23 @@ namespace Bowerbird.Types
         {
         }
 
+        public void UpdateGeometry()
+        {
+            Geometry = Value.ToCurve(RhinoDoc.ActiveDoc.ModelAbsoluteTolerance);
+            ClippingBox = Geometry.GetBoundingBox(false);
+        }
+
+        public Curve Geometry { get; private set; }
+
         public override bool IsValid => Value != null;
 
         public override string TypeName => "BBCurveOnSurface";
 
         public override string TypeDescription => "";
+
+        public BoundingBox ClippingBox { get; private set; }
+
+        public override BoundingBox Boundingbox => ClippingBox;
 
         public override IGH_Goo Duplicate()
         {
@@ -55,5 +71,49 @@ namespace Bowerbird.Types
         }
 
         public override string ToString() => Value.ToString();
+
+        public void DrawViewportWires(GH_PreviewWireArgs args)
+        {
+            if (Geometry == null)
+                UpdateGeometry();
+
+            args.Pipeline.DrawCurve(Geometry, args.Color, args.Thickness);
+        }
+
+        public void DrawViewportMeshes(GH_PreviewMeshArgs args)
+        {
+        }
+
+        public bool BakeGeometry(RhinoDoc doc, ObjectAttributes att, out Guid obj_guid)
+        {
+            obj_guid = doc.Objects.AddCurve(Geometry, att);
+            return obj_guid != Guid.Empty;
+        }
+
+        public override IGH_GeometricGoo DuplicateGeometry()
+        {
+            return new GH_CurveOnSurface(Value);
+        }
+
+        public override BoundingBox GetBoundingBox(Transform xform)
+        {
+            return xform.TransformBoundingBox(ClippingBox);
+        }
+
+        public override IGH_GeometricGoo Transform(Transform xform)
+        {
+            var surface = (Surface)Value.Surface.Duplicate();
+            var curve = Value.Curve;
+            surface.Transform(xform);
+            return new GH_CurveOnSurface(CurveOnSurface.Create(surface, curve));
+        }
+
+        public override IGH_GeometricGoo Morph(SpaceMorph xmorph)
+        {
+            var surface = (Surface)Value.Surface.Duplicate();
+            var curve = Value.Curve;
+            xmorph.Morph(surface);
+            return new GH_CurveOnSurface(CurveOnSurface.Create(surface, curve));
+        }
     }
 }
