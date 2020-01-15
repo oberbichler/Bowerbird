fb0617930bb6c86e47f50ed13ef8fad15aa201e4
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-01-15 14:03:20 +0100

    Fix modifier

--- Bowerbird/CurveOnSurface/GeodesicTorsionComponent.cs
+++ Bowerbird/CurveOnSurface/GeodesicTorsionComponent.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Bowerbird
 {
-    private class GeodesicTorsionComponent : GH_Component
+    class GeodesicTorsionComponent : GH_Component
     {
         public GeodesicTorsionComponent() : base("BB Geodesic Torsion CurveOnSurface", "τg", "", "Bowerbird", "Curve on Surface")
         {
